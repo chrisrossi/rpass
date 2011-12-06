ff2879b2dcbb05051345eb6bc009c37163de329d
Chris Rossi <chris@archimedeanco.com>
2011-12-06 12:07:14 -0500

    Initial commit.

---
+++ rpass.c
@@ -0,0 +1,27 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+int main(void) {
+    int i, l;
+    char table[62];
+    char pass[11];
+    
+    for ( i = 0; i < 26; i++ ) {
+        table[i] = 'a' + i;
+        table[i+26] = 'A' + i;
+    }
+    for ( i = 0; i < 10; i++ ) {
+        table[i+52] = '0' + i;
+    }
+    
+    srand((unsigned)time(NULL));
+    
+    l = 6 + rand() % 5;
+    for ( i = 0; i < l; i++ ) {
+        pass[i] = table[rand()%62];
+    }
+    pass[l] = '\0';
+    
+    printf("%s\n", pass);
+}
